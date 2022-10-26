fa5ec385121c6e46b0ca9f535a235a5c79850604
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-10-26 11:16:45 +0600

    added category api

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const categories =require("./data/courseName.json")
 app.get('/', (req, res) => {
     res.send('News API Running');
+});
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
 });
 
 app.listen(port, () => {
